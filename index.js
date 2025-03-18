d97abe933bf364b250bc081b62d6c712ff78c11b
shox404 <thesh8080@gmail.com>
2025-03-18 10:33:56 +0500

    Update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require("cors");
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(cors({
+    origin: 'https://api-blond-tau.vercel.app',
+    credentials: true
+}));
 
 app.get('/', (req, res) => {
     console.log("Cookies: ", req.cookies);
